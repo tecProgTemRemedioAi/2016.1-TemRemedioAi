40f61d9b905cc0c430be9d556384b156315aca71
Cecilia <ceciliabessa@hotmail.com>
2016-10-26 16:01:23 -0200

    Comment of function and individual comment in AboutActivity class

--- TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/AboutActivity.java
+++ TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/AboutActivity.java
@@ -11,9 +11,16 @@ import com.tra.gppmds.temremdioa.R;
 
 public class AboutActivity extends AppCompatActivity {
 
+    /**
+     * Method: onCreate()
+     * Purpose: create the activity on the screen.
+     * @return
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        // Make visible the activity with information about the application, not editable
         setContentView(R.layout.activity_about);
     }
 }
