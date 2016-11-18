4f35439b56c369fd0116feceb11254d53409d2c7
Carolina Ferreira <carol.mf14@gmail.com>
2016-11-18 12:01:34 -0200

    Comment variable declaration

--- TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/adapter/TabsAdapter.java
+++ TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/adapter/TabsAdapter.java
@@ -14,7 +14,7 @@ import com.gppmds.tra.temremdioa.controller.fragment.UBSFragment;
 
 public class TabsAdapter extends FragmentPagerAdapter{
 
-    private static final int tabsQuantity = 2;
+    private static final int tabsQuantity = 2;  // This variable refers to the number of tabs present in the application.
 
     public TabsAdapter(FragmentManager fm){
         super(fm);
@@ -41,15 +41,24 @@ public class TabsAdapter extends FragmentPagerAdapter{
         return null;
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public int getCount() {
         return tabsQuantity;
     }
 
+    /**
+     *
+     * @param position
+     * @return
+     */
     @Override
     public CharSequence getPageTitle(int position) {
-        final int DOWN_LIMIT = 0;
-        final int UP_LIMIT = 1;
+        final int DOWN_LIMIT = 0;   // this variable refers to the down limit positon to set name of tab.
+        final int UP_LIMIT = 1;     // this variable refers to the up limit position to set name of tab.
 
         assert(position >= DOWN_LIMIT && position <= UP_LIMIT);
 
