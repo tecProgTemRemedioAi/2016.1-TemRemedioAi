e8f5dcc638ba880782877c76b62717119095653f
Carolina Ferreira <carol.mf14@gmail.com>
2016-11-21 11:32:03 -0200

    Emphasis for more important code

--- TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/SplashScreenActivity.java
+++ TemRemedioAi/app/src/main/java/com/gppmds/tra/temremdioa/controller/SplashScreenActivity.java
@@ -1,3 +1,7 @@
+/**
+ * File: SplachScreenActivity.java
+ * Purpose: this file set all paramters about the splash screen on app.
+ */
 package com.gppmds.tra.temremdioa.controller;
 
 import android.app.Activity;
@@ -14,6 +18,26 @@ public class SplashScreenActivity extends Activity {
 
     private static final int waitTime = 2000;
 
+    /**
+     * Method: openMainActivity
+     * Purpose: this method open the MainActivity after the splash screen was loaded.
+     * @return boolean
+     */
+    public boolean openMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        try {
+            startActivity(intent);
+            return true;
+        } catch (ActivityNotFoundException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Method: onCreate
+     * Purpose: this method instance the content view for splasj screen.
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,15 +50,5 @@ public class SplashScreenActivity extends Activity {
                 finish();
             }
         }, waitTime);
-    }
-
-    public boolean openMainActivity() {
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        try {
-            startActivity(intent);
-            return true;
-        } catch (ActivityNotFoundException e) {
-            return false;
-        }
     }
 }
